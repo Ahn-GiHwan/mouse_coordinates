609b917f0d20466efb46489d76225bef047d86b2
Ahn-GiHwan <ahngh0113@naver.com>
2021-08-14 18:39:11 +0900

    feat: change logic

--- main.js
+++ main.js
@@ -7,10 +7,13 @@ window.addEventListener("mousemove", (e) => {
   const x = e.clientX;
   const y = e.clientY;
 
-  tag.innerHTML = `x: ${x}, y: ${y}`;
   width.style.top = `${y}px`;
   height.style.left = `${x}px`;
 
   target.style.top = `${y}px`;
   target.style.left = `${x}px`;
+
+  tag.style.top = `${y}px`;
+  tag.style.left = `${x}px`;
+  tag.innerHTML = `x: ${x}, y: ${y}`;
 });
